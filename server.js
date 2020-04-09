2c71be35a89e459428675f438e12eb370820df8c
gavin stahl <stahlgavin@gmail.com>
2020-04-09 14:23:23 -0700

    mvp completed

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
+// const morgan = require("morgan"); // remember to require the module after installing it
+const helmet = require("helmet");
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 const server = express();
+
+// middleware
+server.use(logger);
+server.use(helmet());
+server.use(express.json()); // built-in middleware, no need to install it
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +18,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,136 @@
 const express = require('express');
-
+const db = require("./userDb");
+const postsDB = require("../posts/postDb")
 const router = express.Router();
 
-router.post('/', (req, res) => {
+// inserts new user
+router.post("/", validateUser, (req, res) => {
   // do your magic!
+  console.log(req.body);
+  db
+    .insert(req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "error adding user"
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+// inserts new post for specific user
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   // do your magic!
+  req.body.user_id = req.params.id;
+  postsDB
+    .insert(req.body)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.get('/', (req, res) => {
+// get all users
+router.get("/", (req, res) => {
   // do your magic!
+  db.get().then(users => {
+    res.status(200).json(users);
+  });
 });
 
-router.get('/:id', (req, res) => {
+// get user by id
+router.get("/:id", validateUserId, (req, res) => {
   // do your magic!
+  db
+    .getById(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
+// get user posts by id
+router.get("/:id/posts", validateUserId, (req, res) => {
   // do your magic!
+  db
+    .getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+// delete a user
+router.delete("/:id", validateUserId, (req, res) => {
   // do your magic!
+  db
+    .remove(req.params.id)
+    .then(user => {
+      res.status(200).json({ message: "user has been removed" });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "user wasnt removed" });
+    });
 });
 
-router.put('/:id', (req, res) => {
+// update a user
+router.put("/:id", validateUserId, (req, res) => {
   // do your magic!
+  console.log(req.body);
+  db
+    .update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "error updating user"
+      });
+    });
 });
 
 //custom middleware
-
 function validateUserId(req, res, next) {
   // do your magic!
+  db
+    .getById(req.params.id)
+    .then(user => {
+      if (user.id) {
+        req.user = user;
+        console.log(req.user);
+      } else {
+        res.status(400).json({ message: "not a valid user id" });
+      }
+    })
+    .catch(error => {
+      res.status(400).json({ message: "invalid user id" });
+    });
+  next();
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  req.body.name
+    ? null
+    : res.status(400).json({ message: "missing required name field" });
+  next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  req.body.text
+    ? null
+    : res.status(400).json({ message: "missing required text field" });
 }
+
 
 module.exports = router;
